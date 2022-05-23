3c53a7fbee552249837b38a1e2752c12c49f4ea4
Mehdi Jafarzadeh <mehdi.jafarzadeh@usu.com>
2022-05-23 17:25:55 +0200

    first commit typescript

---
+++ app.js
@@ -0,0 +1,9 @@
+var person = {
+    name: "Mehdi",
+    age: 39,
+    hobbies: ["sports", "cooking"]
+};
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
---
+++ basics.ts
@@ -0,0 +1,11 @@
+function addTwo(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+const num1 = 5;
+const num2 = 2.8;
+
+const result = addTwo(num1, num2);
+console.log(result);
+
+//export {};
